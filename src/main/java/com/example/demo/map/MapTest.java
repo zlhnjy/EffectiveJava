2aedb9772fd9a95d89febcc5f3a2316ab1852919
zhangliang26 <zhangliang26@meituan.com>
2019-04-30 11:37:00 +0800

    update class MapTest

--- src/main/java/com/example/demo/map/MapTest.java
+++ src/main/java/com/example/demo/map/MapTest.java
@@ -14,6 +14,10 @@ import java.util.Set;
 public class MapTest {
 
     public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
         Map<String, String> map = new HashMap<>();
         map.put("aaa", "111");
         map.put("bbb", "222");
